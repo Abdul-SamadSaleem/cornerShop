6d1e4033c1d22632ea8185dda648739f585298f4
Abdul-SamadSaleem <abdullahplayz07@gmail.com>
2020-03-01 19:01:00 +0000

    made a navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img
+        src="https://data.whicdn.com/images/66497755/original.gif"
+        alt="supreme logo"
+        className="logo"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
