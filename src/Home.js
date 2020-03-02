48016b05b291508bed4f456e16efda0226dcdcc5
Abdul-SamadSaleem <abdullahplayz07@gmail.com>
2020-03-02 17:39:06 +0000

    added a hokme page with a bit of styling

---
+++ src/Home.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <h2 className="home_header">Welcome to Supreme</h2>
+      <p className="home__desc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Volutpat consequat
+        mauris nunc congue. Pellentesque id nibh tortor id aliquet lectus.
+        Tortor consequat id porta nibh venenatis cras sed. Ipsum dolor sit amet
+        consectetur. Egestas fringilla phasellus faucibus scelerisque eleifend
+        donec. Laoreet non curabitur gravida arcu ac tortor dignissim. Quam
+        viverra orci sagittis eu volutpat odio. Diam sit amet nisl suscipit.
+        Diam quis enim lobortis scelerisque fermentum dui faucibus.
+      </p>
+      <p className="home__desc">
+        Facilisis mauris sit amet massa. Neque laoreet suspendisse interdum
+        consectetur libero id. Et malesuada fames ac turpis egestas sed tempus
+        urna et. Nulla pellentesque dignissim enim sit amet. Cras adipiscing
+        enim eu turpis egestas pretium aenean. Congue mauris rhoncus aenean vel
+        elit scelerisque. Diam maecenas ultricies mi eget. Vestibulum sed arcu
+        non odio euismod. Quis blandit turpis cursus in. Nec tincidunt praesent
+        semper feugiat nibh sed pulvinar proin gravida. Vitae semper quis lectus
+        nulla at volutpat. Sed lectus vestibulum mattis ullamcorper velit.
+        Auctor urna nunc id cursus metus aliquam eleifend mi in. Eu feugiat
+        pretium nibh ipsum.
+      </p>
+      <p className="home__desc">
+        Quisque id diam vel quam elementum. Suspendisse sed nisi lacus sed
+        viverra tellus in hac. Venenatis tellus in metus vulputate eu. Sem
+        fringilla ut morbi tincidunt augue. Convallis posuere morbi leo urna.
+        Mauris cursus mattis molestie a iaculis at erat. Pellentesque habitant
+        morbi tristique senectus et netus et malesuada. Pulvinar etiam non quam
+        lacus suspendisse. Consequat semper viverra nam libero justo laoreet sit
+        amet. Venenatis tellus in metus vulputate. Ut pharetra sit amet aliquam
+        id. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis
+        orci. Eros donec ac odio tempor orci dapibus ultrices in iaculis.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
