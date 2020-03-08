20751718476c60aba5bff48ebb4c533c00e06a80
Abdul-SamadSaleem <abdullahplayz07@gmail.com>
2020-03-08 13:31:52 +0000

    added a audio and item counter

--- src/Item.js
+++ src/Item.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import UIfx from "uifx";
 import "./Item.css";
+import cash from "./cash.mp3";
 
 const Item = props => {
+  const beep = new UIfx(cash);
+
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div className="item">
+      <h2>{itemCount}</h2>
       <img src={props.img} alt={props.alt} className="itemPic" />
       <h1 className="item__desc">{props.desc}</h1>
-      <button>+</button>
+      <button
+        onClick={() => {
+          beep.play();
+          setItemCount(itemCount + 1);
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
