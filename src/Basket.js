746548cb1a73387e134f8e2828d6192093e2f513
Abdul-SamadSaleem <abdullahplayz07@gmail.com>
2020-03-08 13:17:15 +0000

    made a basket file

---
+++ src/Basket.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Basket.css";
+
+const Basket = () => {
+  return (
+    <div>
+      <h1 className="basket__title">This is your basket</h1>
+    </div>
+  );
+};
+
+export default Basket;
